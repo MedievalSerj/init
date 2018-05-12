8971904e9f4d6a0e83c7ad123c641b9a17cf4a4f
MedievalSerj <ladonya.s@gmail.com>
2018-05-12 15:38:47 +0300

    add 02

---
+++ scripts/02.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+script_file=script_file
+logs_file=/var/log/update_script.log
+
+touch $script_file
+
+tee $script_file <<FUCK
+#!/bin/bash
+function update_upgrade() {
+  sudo apt-get update 2>&1 >> $logs_file
+  sudo apt-get upgrade 2>&1 >> $logs_file
+}
+update_upgrade
+FUCK
+
+chmod 777 script_file
+
+script_path=$(readlink -f file.txt)
+
+(crontab -l ; echo "0 4 * * * $script_path")| crontab -
