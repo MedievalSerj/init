4e1c3e17663924be481dd3f829b97d5e1fdf755a
Serhii Ladonia <ladonya.s@gmail.com>
2018-05-19 20:00:13 +0300

    remove sudo from scripts

--- scripts/02.sh
+++ scripts/02.sh
@@ -6,8 +6,8 @@ crontab_line="0 4 * * 0 ${this_script_paht}"
 line_in_crontab_file=`crontab -l | grep -F "$crontab_line"`
 
 function update_upgrade() {
-  sudo apt-get update 2>&1 >> $logs_file
-  sudo apt-get upgrade 2>&1 >> $logs_file
+  apt-get update 2>&1 >> $logs_file
+  apt-get upgrade 2>&1 >> $logs_file
 }
 
 if [ -z "$line_in_crontab_file" ] ; then
